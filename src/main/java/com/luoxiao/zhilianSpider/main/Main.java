f7e68ad11a34e1f4a407a5d4921afdf630b541e0
Mandelo <loshower@gmail.com>
2017-03-27 00:03:57 +0800

    新增功能:生成excel文件

--- src/main/java/com/luoxiao/zhilianSpider/main/Main.java
+++ src/main/java/com/luoxiao/zhilianSpider/main/Main.java
@@ -1,7 +1,9 @@
 package com.luoxiao.zhilianSpider.main;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
+import com.luoxiao.zhilianSpider.utils.ExportTools;
 import com.luoxiao.zhilianSpider.utils.ParseJobTools;
 
 /** 
@@ -14,11 +16,15 @@ public class Main {
 	 * @param args 设置关键字 职业 和 地区
 	 * @throws SQLException 
 	 * @throws ClassNotFoundException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ParseJobTools tools = new ParseJobTools();
-		//搜索关键字:武汉 java
-		tools.praseContent("JAVA", "武汉");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+		ParseJobTools parseTools = new ParseJobTools();
+		ExportTools exportTools = new ExportTools();
+		//搜索关键字:"java" "武汉"
+		parseTools.praseContent("JAVA", "武汉");
+		//从数据库查询数据,并生成eacel文件
+		exportTools.exportExcel();
 	}
 
 }
---
+++ src/main/java/com/luoxiao/zhilianSpider/utils/ExportTools.java
@@ -0,0 +1,104 @@
+package com.luoxiao.zhilianSpider.utils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+/** 
+* @Description: 生成excel文件相关
+* @author luoxiao
+* @date 2017年3月27日 
+*/
+public class ExportTools {
+
+    Connection connection;
+    Statement stmt;
+
+    public void exportExcel() throws IOException {
+        String sql = "SELECT * FROM job_info;";
+        try {
+            connection = new ConnectionTools().getConnection();
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            @SuppressWarnings("resource")
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("zhilian");
+            // 在sheet里创建第一行，参数为行索引(excel的行)，可以是0～65535之间的任何一个
+            HSSFRow row = sheet.createRow((short) 0);
+            HSSFCell cell = null;
+            cell = row.createCell((short) 0);
+            cell.setCellValue("编号");
+            cell = row.createCell((short) 1);
+            cell.setCellValue("公司名称");
+            cell = row.createCell((short) 2);
+            cell.setCellValue("详情页");
+            cell = row.createCell((short) 3);
+            cell.setCellValue("月薪");
+            cell = row.createCell((short) 4);
+            cell.setCellValue("地址");
+            cell = row.createCell((short) 5);
+            cell.setCellValue("发布日期");
+            // 写入数据,i为行号
+            int i = 1;
+            System.out
+                    .println("---------------------------------------开始写入数据----------------------------------------------");
+            while (rs.next()) {
+                row = sheet.createRow(i);
+                cell = row.createCell(0);
+                cell.setCellValue(rs.getString("id"));
+                cell = row.createCell(1);
+                cell.setCellValue(rs.getString("companyName"));
+                cell = row.createCell(2);
+                cell.setCellValue(rs.getString("url"));
+                cell = row.createCell(3);
+                cell.setCellValue(rs.getString("wages"));
+                cell = row.createCell(4);
+                cell.setCellValue(rs.getString("location"));
+                cell = row.createCell(5);
+                cell.setCellValue(rs.getString("date"));
+                i++;
+            }
+            String date = formatDate(new Date());
+            String outputFile = "C:\\智联招聘  " + date + ".xls";
+            FileOutputStream FOut = new FileOutputStream(outputFile);
+            workbook.write(FOut);
+            FOut.flush();
+            FOut.close();
+            stmt.close();
+            connection.close();
+            System.out.println("----------------------------------------文件" + outputFile
+                    + "创建完成----------------------------------");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ExportTools e = new ExportTools();
+        e.exportExcel();
+    }
+
+    /**
+     * @Description: 格式化当前日期
+     * @param:
+     * @return:
+     * @throws
+     */
+    public String formatDate(Date date) {
+        date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String result = format.format(date);
+        return result;
+    }
+
+}
